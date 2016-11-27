963df407dcd7ccb6e79639d905ce43a6f855f982
Veggie <cmderochie+github@gmail.com>
2016-11-27 15:21:33 -0700

    Initial commit

---
+++ VideoCompression/Image.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoCompression
+{
+    public class Image
+    {
+        private Pixel[,] _pixels;
+
+        public Pixel this[int x, int y]
+        {
+            get { return _pixels[x, y]; }
+        }
+    }
+}
---
+++ VideoCompression/Pixel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoCompression
+{
+    public struct Pixel
+    {
+        public byte R;
+        public byte G;
+        public byte B;
+    }
+}
